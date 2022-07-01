74c86738aa4a33e95797b2d1a13cfee52d80c367
vicorandy <vicorandyrichard26@gmail.com>
2022-07-01 11:32:42 +0000

    pa

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 97; i < 123; i++)
 	{
-		if (alphabet[i] != "q" && alphabet[i] != "e")
+		if (i != 101 && i != 113)
 		{
-			putchar(alphabet[i]);
+			putchar(i);
 		}
 	}
 	putchar('\n');

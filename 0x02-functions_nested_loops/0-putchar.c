1e4399366fb9d423bcd7c3f85547037b32604a62
vicorandy <vicorandyrichard26@gmail.com>
2022-07-05 08:14:08 +0000

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+#include "main.h"
+
+/**
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  int arr[ 8 ] = {95,112,117,116,99,104,97,114};
+	int i, sz = 8;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(arr[i]);
+	}
+		_putchar('/n');
+	return (0);
+}

836dfeab7e3b01cb856810278f4e126aa6cbce13
vicorandy <vicorandyrichard26@gmail.com>
2022-07-05 08:50:52 +0000

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+* main - Prints all Alphabets in lower case.
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('/n');
+}

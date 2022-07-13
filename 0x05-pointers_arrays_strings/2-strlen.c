7aa3b2c481f04168be010a1144868c7b84f7ae36
vicorandy <vicorandyrichard26@gmail.com>
2022-07-13 15:05:40 +0000

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int size;
+
+	size = sizeof(s);
+	return (size);
+}
